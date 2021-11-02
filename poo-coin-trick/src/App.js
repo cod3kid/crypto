67c8006244405e1f479c62dcb2c535639633b32f
cod3kid <djsufail@gmail.com>
2021-11-02 21:13:08 +0530

    Full Done

--- poo-coin-trick/src/App.js
+++ poo-coin-trick/src/App.js
@@ -1,84 +1,64 @@
 import { useState } from "react";
 import axios from "axios";
+import { fourthLastDigit } from "./utils";
+import "./app.css";
 
 function App() {
   const [firstValue, setFirstValue] = useState("");
   const [lastValue, setLastValue] = useState("");
   const [listCoins, setListCoins] = useState([]);
-  const fourthLastDigit = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
 
   const fetchCoin = () => {
+    setListCoins([]);
     fourthLastDigit.forEach(async (el) => {
       await axios
         .get(`https://api1.poocoin.app/tokens?search=${el}${lastValue}`)
         .then((res) => {
-          setListCoins((prev) => [...prev, ...res.data]);
+          const filteredCoins = res.data.filter((coin) =>
+            coin.address.startsWith(firstValue)
+          );
+          setListCoins((prev) => [...prev, ...filteredCoins]);
         });
     });
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={firstValue}
-        onChange={(e) => setFirstValue(e.target.value)}
-      />
-      <input
-        type="text"
-        value={lastValue}
-        onChange={(e) => setLastValue(e.target.value)}
-      />
-
-      <button onClick={fetchCoin}>Submit</button>
-      <ul>
-        {listCoins.length &&
-          listCoins.map((item) => {
+      <div style={{ display: "flex", justifyContent: "center", padding: 30 }}>
+        <input
+          type="text"
+          placeholder="First Few Characters"
+          value={firstValue}
+          onChange={(e) => setFirstValue(e.target.value)}
+          style={{ marginRight: 10 }}
+        />
+        <input
+          type="text"
+          placeholder="Last Few Characters"
+          value={lastValue}
+          onChange={(e) => setLastValue(e.target.value)}
+          style={{ marginRight: 10 }}
+        />
+        <button onClick={fetchCoin}>Search</button>
+      </div>
+      {listCoins.length ? (
+        <table>
+          <tr>
+            <th>Symbol</th>
+            <th>Coin</th>
+            <th>address</th>
+          </tr>
+          {listCoins.map((item) => {
             return (
-              <li>
-                {item.name}
-                {"  "} {item.address}
-              </li>
+              <tr>
+                <td>{item.symbol}</td>
+                <td>{item.name}</td>
+                <td>{item.address}</td>
+              </tr>
             );
           })}
-      </ul>
+        </table>
+      ) : null}
     </div>
   );
 }
