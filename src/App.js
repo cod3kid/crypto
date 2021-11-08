62906a91a893d373d536b5631530b6367c1bc740
cod3kid <djsufail@gmail.com>
2021-11-08 23:41:19 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { fourthLastDigit } from "./utils";
 import "./app.css";
 
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
 
 function App() {
   const [firstValue, setFirstValue] = useState("");
